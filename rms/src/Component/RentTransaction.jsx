72ee877418c8f8a03febf71384097b3c09f420f3
SansarTiwari <st9889477@gmail.com git config --global user.name SansarTiwari>
2024-10-05 13:51:58 +0530

    "Updated package.json, App.js, ListRentMaster.jsx, and RentTransaction.jsx files with changes to HTTPS settings, route paths, and form fields."

--- rms/src/Component/RentTransaction.jsx
+++ rms/src/Component/RentTransaction.jsx
@@ -1,11 +1,64 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import {
+  selectAllPropertyMaster,
+} from "../Redux/Slice/userSlice";
+import { useParams } from 'react-router'
+import axios from 'axios';
 
 function RentTransaction() {
 
   const [formData, setFormData] = useState({
     rentFrom: '',
-    rentTo: ''
+    rentTo: '',
+    pincode: "",
+    address2: "",
+    city: "",
+    address1: "",
+    state: "",
+    propertymaster: "",
+    ownerMasters: '',
+    monthlyRent: "",
   })
+  const url = new URLSearchParams(window.location.search)
+
+  const id = url.get('Id')
+
+  const { whichroute } = useParams();
+
+  const [message, setMessage] = useState({
+    success: "",
+    danger: "",
+  });
+
+  useEffect(() => {
+
+    const getData = async () => {
+      try {
+        const response = await axios.get(`https://rsmapi.vercel.app/${whichroute}/${id}`, {
+          headers: { Authorization: `Bearer ${localStorage.getItem('token')}` }
+        })
+        if (response.status == 200) {
+          setFormData(await response.data)
+        }
+        console.log(response);
+      } catch (error) {
+        setMessage({
+          ...message,
+          danger: `${error.message}, While retriving RentMaster`,
+        });
+      } finally {
+        setTimeout(
+          () =>
+            setMessage({
+              success: "",
+              danger: "",
+            }),
+          3000
+        );
+      }
+    }
+    getData()
+  }, [id, whichroute])
 
   const handleData = (e) => {
     e.preventDefault();
@@ -35,7 +88,6 @@ function RentTransaction() {
               <div className="help-block with-errors"></div>
             </div>
           </div>
-
           <div className="col-md-6">
             <div className="form-group">
               <label>Rent To *</label>
@@ -48,6 +100,105 @@ function RentTransaction() {
                 value={formData.rentTo}
                 onChange={handleData}
                 required=""
+              />
+              <div className="help-block with-errors"></div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label>Pincode*</label>
+              <input
+                type="text"
+                className="form-control"
+                name="pincode"
+                value={formData.propertymaster && formData.propertymaster.pincode.$numberDecimal}
+                onChange={handleData}
+                required=""
+                disabled
+              />
+              <div className="help-block with-errors"></div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label>Address1*</label>
+              <input
+                type="text"
+                className="form-control"
+                name="address1"
+                value={formData.propertymaster && formData.propertymaster.address1}
+                onChange={handleData}
+                disabled
+              />
+              <div className="help-block with-errors"></div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label>Address2*</label>
+              <input
+                type="text"
+                className="form-control"
+                name="address2"
+                value={formData.propertymaster && formData.propertymaster.address2}
+                onChange={handleData}
+                disabled
+              />
+              <div className="help-block with-errors"></div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label>City*</label>
+              <input
+                type="text"
+                className="form-control"
+                name="city"
+                value={formData.propertymaster && formData.propertymaster.city}
+                onChange={handleData}
+                disabled
+              />
+              <div className="help-block with-errors"></div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label>State*</label>
+              <input
+                type="text"
+                className="form-control"
+                name="state"
+                value={formData.propertymaster && formData.propertymaster.state}
+                onChange={handleData}
+                disabled
+              />
+              <div className="help-block with-errors"></div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label>MonthlyRent*</label>
+              <input
+                type="text"
+                className="form-control"
+                name="monthlyRent"
+                value={formData.monthlyRent.$numberDecimal}
+                onChange={handleData}
+                disabled
+              />
+              <div className="help-block with-errors"></div>
+            </div>
+          </div>
+          <div className="col-md-6">
+            <div className="form-group">
+              <label>OwnerMaster*</label>
+              <input
+                type="text"
+                className="form-control"
+                name="ownermaster"
+                value={formData.propertymaster && formData.propertymaster.ownerMasters}
+                onChange={handleData}
+                disabled
               />
               <div className="help-block with-errors"></div>
             </div>
